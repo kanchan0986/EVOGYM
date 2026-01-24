cb572e9b6310dc964da941d512e236a5797740e7
Kanchan Sanyal <kanchan_sanyal11@yahoo.com>
2026-01-25 02:37:56 +0530

    netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from 'path';
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyReactRouter()],
   resolve: {
     alias: [
       {find: "@", replacement: path.resolve(__dirname, "app")}
